ae18718be13d7e4a7347b90d3a68ea499d0f4b1d
Sugam Shrestha <sugamsht143@gmail.com>
2021-06-15 17:27:38 +0545

    Added gfm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import { defaultMarkdown } from './DefaultMarkdown';
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// ReactMarkdown.setOptions({
+//   breaks: true
+// });
+
+
+function App (){
+  
+  const[text,setText] = useState(defaultMarkdown)
+  
+    function onChange (e) {
+      setText(e.target.value)
+  };
+
+  return(
+   <div className="App">
+        <h1 id="title">React Markdown Previewer</h1>
+        <textarea 
+          id="editor" 
+          value={text} 
+          onChange={onChange} 
+          placeholder="Enter Markdown..." />
+        <div id="preview">
+            <ReactMarkdown remarkPlugins={[gfm]} children={text} />
+        </div> 
+      </div>
+  )
 }
+
 
 export default App;
